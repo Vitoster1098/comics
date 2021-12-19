d1e3bdea066ed91f5060ceff676a4f8442c20b22
vitaliy <zzzvitzzz4@gmail.com>
2021-12-19 18:35:32 +0300

    Git init

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная";
+$arDirProperties = array(
+   "description" => "Блоги. Форумы. Фотогалерея. Комиксы. Общение.",
+   "keywords" => "блоги, форумы, фотогалерея, комиксы, жанры.",
+   "robots" => "index, follow"
+);
+?>
